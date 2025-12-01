382a3c3be433ba6344d485ba452d5c52e4b65887
Nicko <cjnickorosales@gmail.com>
2025-12-01 15:13:32 +0800

    make the cart quantity interractive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,7 +80,16 @@ products.forEach((product)=>{
                     quantity: 1
                 });
             }
+
+            let cartQuantity = 0;
+            cart.forEach((item)=> {
+                cartQuantity += item.quantity
+            })
+
+            document.querySelector('.js-cart-quantity').
+            innerHTML = cartQuantity;
             
-            console.log(cart)
+            
+            
         });
     })
