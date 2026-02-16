375e87954ec92edd324f9fa46d9cd5e15b6bfa3d
Nicko <cjnickorosales@gmail.com>
2026-02-16 11:51:08 +0800

    Make delivery options interractive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, updateQuantity } from '../data/cart.js';
+import { cart, removeFromCart, updateQuantity, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js"
@@ -109,7 +109,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId
         html += 
         `
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option"
+                data-product-id="${matchingProduct.id}"
+                data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio"
                 ${isChecked ? 'checked': ""}
                 class="delivery-option-input"
@@ -184,3 +186,11 @@ function updateCartQuantity(){
   document.querySelector('.js-return-to-home-link').
   innerHTML = (cartQuantity===1 ? cartQuantity + " Item" : cartQuantity + " Items");
 }
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element)=>{
+        element.addEventListener('click',()=>{
+            const {productId, deliveryOptionId}= element.dataset;
+            updateDeliveryOption(productId, deliveryOptionId)
+        })
+    })
